c90d23ce1ab6061eaea1bf292d49c6a73aeeaee0
Aaron Ho <bicecreamaar@gmail.com>
2023-05-29 20:55:34 -0700

    finished

--- script.js
+++ script.js
@@ -22,31 +22,33 @@ function generatePassword() {
       passwordLength = prompt("Number is not between 8 and 128. Please try again.");
     }
     
-    var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L","M","N","O","P","Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-    var numbers = ["0","1","2","3","4","5","6","7","8","9"];
-    var specialCharacters  = ["!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","}","[","]","|","\\",":",";","\"","'","<",">","?","/","~","`"];
+    var lowercase = ["abcdefghijklmnopqrstuvwxyz"];
+    var uppercase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+    var numbers = ["0123456789"];
+    var specialCharacters  = ["!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"];
 
-
-    // todo: fix this
     do {
       var characterTypes = [];
       var possibleCharacters = 0;
       if(confirm("Would you like to include lowercase letters?")) {
         characterTypes += lowercase;
         possibleCharacters += 26;
+        console.log("lowercase " + characterTypes);
       }
       if(confirm("Would you like to include uppercase letters?")) {
         characterTypes += uppercase;
         possibleCharacters += 26;
+        console.log("uppercase " + characterTypes);
       }
       if(confirm("Would you like to include numbers?")) {
         characterTypes += numbers;
         possibleCharacters += 10;
+        console.log("numbers " + characterTypes);
       }
       if(confirm("Would you like to include special characters?")) {
         characterTypes += specialCharacters;
         possibleCharacters += 32;
+        console.log("special " + characterTypes);
       }
       // Check if at least one character type is selected
       if (characterTypes.length === 0) {
@@ -54,21 +56,13 @@ function generatePassword() {
       }
     } while(characterTypes.length === 0);
   
-
-    // todo: fix this
     // Create password
     var password = "";
     for (var i = 0; i < passwordLength; i++) {
-      var random = Math.floor(Math.random() * possibleCharacters.length);
-      if(random < 26) { // lowercase
-        password += lowercase[random];
-      } else if(random < 52) { // uppercase 
-        password += uppercase[random + 26];
-      } else if(random < 62) { // numbers
-        password += numbers[random + 52];
-      } else { // special characters
-        password += specialCharacters[random + 62];
-      }
+      var random = Math.floor(Math.random() * possibleCharacters);
+      password += characterTypes[random];
+      console.log(characterTypes.length);
+      console.log(" charactertype[" + random + "] = " + characterTypes[random] + "\n i : " + i + " : " + password);
     }
   
     // Return password
